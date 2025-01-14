93d519c0534c9cc45852b5416bc5fa399d067e1c
GustavoACasanNunez <115280027+GustavoACasanNunez@users.noreply.github.com>
2025-01-14 11:45:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/palette/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.palette
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.appbar)
+        setSupportActionBar(toolbar)
+
+        val items = ArrayList<Tarjeta>()
+        items.add(Tarjeta(R.drawable.image1))
+        items.add(Tarjeta(R.drawable.image2))
+        items.add(Tarjeta(R.drawable.image3))
+        items.add(Tarjeta(R.drawable.image4))
+        items.add(Tarjeta(R.drawable.image5))
+        items.add(Tarjeta(R.drawable.image6))
+        items.add(Tarjeta(R.drawable.image7))
+        items.add(Tarjeta(R.drawable.image8))
+
+        val recView = findViewById<RecyclerView>(R.id.recview)
+
+        recView.setHasFixedSize(true)
+
+        val adaptador = CardsAdapter(items)
+        recView.adapter = adaptador
+        recView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        adaptador.onClick  = {
+        }
+    }
+}
